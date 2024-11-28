ff7d2ccf67b33374c7a544f64729572f1e56e4df
monar24 <mona@oneof.com>
2024-11-28 11:17:24 -0500

    push additions

---
+++ config.ts
@@ -0,0 +1,8 @@
+
+require("dotenv").config();
+
+
+export const HELLO_COOKIE_SECRET = process.env.HELLO_COOKIE_SECRET as string;
+export const CLIENT_ID = process.env.CLIENT_ID as string;
+export const HOST = process.env.HOST as string;
+export const APP_URL = process.env.APP_URL as string;
---
+++ hello.config.ts
@@ -0,0 +1,47 @@
+import { CLIENT_ID, HELLO_COOKIE_SECRET, APP_URL } from "./config.ts";
+
+type ScopeType = (
+    | "name"
+    | "nickname"
+    | "preferred_username"
+    | "given_name"
+    | "family_name"
+    | "email"
+    | "phone"
+    | "picture"
+    | "ethereum"
+    | "discord"
+    | "twitter"
+    | "github"
+    | "gitlab"
+    | "profile"
+    | "openid"
+    | "profile_update"
+  )[];
+
+type ProviderHint =(
+    | 'github'
+    | 'apple--'
+    | 'google'
+    | 'microsoft'
+    | 'discord'
+    | 'twitter'
+    | 'facebook'
+    | 'ethereum'
+    | 'qrcode') [];
+
+export const helloConfig = {
+    client_id: CLIENT_ID,
+    secret: HELLO_COOKIE_SECRET || 'fallback_secret',
+    scope: ['openid','name','email','picture'] as ScopeType, // default - openid name email picture
+    provider_hint: ['github', 'apple--'] as ProviderHint, 
+    routes: {
+        loggedIn: '/',
+        loggedOut: '/logout',
+        error: '/error',
+    },
+    redirectURI: APP_URL
+    // logConfig?: boolean;
+    // apiRoute?: string;
+}
+
